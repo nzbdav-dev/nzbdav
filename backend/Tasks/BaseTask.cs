c39010f78fde8420dea0a96865e913b0763cd6c7
nzbdav-dev <nzbdavdev@protonmail.com>
2025-10-02 07:34:17 -0700

    [Refactor] MigrateLibrarySymlinkTask and BaseTask

---
+++ backend/Tasks/BaseTask.cs
@@ -0,0 +1,33 @@
+﻿namespace NzbWebDAV.Tasks;
+
+public abstract class BaseTask
+{
+    protected abstract Task ExecuteInternal();
+
+    private static readonly SemaphoreSlim Semaphore = new(1, 1);
+    private static Task? _runningTask;
+
+    public async Task<bool> Execute()
+    {
+        await Semaphore.WaitAsync();
+        Task? task;
+        try
+        {
+            // if the task is already running, return immediately.
+            if (_runningTask is { IsCompleted: false })
+                return false;
+
+            // otherwise, run the task.
+            _runningTask = Task.Run(ExecuteInternal);
+            task = _runningTask;
+        }
+        finally
+        {
+            Semaphore.Release();
+        }
+
+        // and wait for it to finish.
+        await task;
+        return true;
+    }
+}
--- backend/Tasks/MigrateLibrarySymlinksTask.cs
+++ backend/Tasks/MigrateLibrarySymlinksTask.cs
@@ -12,36 +12,9 @@ public class MigrateLibrarySymlinksTask(
     ConfigManager configManager,
     WebsocketManager websocketManager,
     DatabaseStore store
-)
+) : BaseTask
 {
-    private static readonly SemaphoreSlim Semaphore = new(1, 1);
-    private static Task? _runningTask;
-
-    public async Task<bool> Execute()
-    {
-        await Semaphore.WaitAsync();
-        Task? task;
-        try
-        {
-            // if the task is already running, return immediately.
-            if (_runningTask is { IsCompleted: false })
-                return false;
-
-            // otherwise, run the task.
-            _runningTask = Task.Run(ExecuteInternal);
-            task = _runningTask;
-        }
-        finally
-        {
-            Semaphore.Release();
-        }
-
-        // and wait for it to finish.
-        await task;
-        return true;
-    }
-
-    private async Task ExecuteInternal()
+    protected override async Task ExecuteInternal()
     {
         try
         {
