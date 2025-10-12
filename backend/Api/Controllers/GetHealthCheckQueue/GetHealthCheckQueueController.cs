6f39debcdf10dd7e3da857ad305b8d5bf7a85812
nzbdav-dev <nzbdavdev@protonmail.com>
2025-10-12 13:38:45 -0700

    Added GetHealthCheckQueue api to backend.

---
+++ backend/Api/Controllers/GetHealthCheckQueue/GetHealthCheckQueueController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NzbWebDAV.Database;
+using NzbWebDAV.Database.Models;
+
+namespace NzbWebDAV.Api.Controllers.GetHealthCheckQueue;
+
+[ApiController]
+[Route("api/get-health-check-queue")]
+public class GetHealthCheckQueueController(DavDatabaseClient dbClient) : BaseApiController
+{
+    private async Task<GetHealthCheckQueueResponse> GetHealthCheckQueue(GetHealthCheckQueueRequest request)
+    {
+        var davItems = await dbClient.Ctx.Items
+            .Where(x => x.Type == DavItem.ItemType.NzbFile || x.Type == DavItem.ItemType.RarFile)
+            .OrderBy(x => x.NextHealthCheck)
+            .ThenByDescending(x => x.ReleaseDate)
+            .Take(request.PageSize)
+            .ToListAsync();
+
+        return new GetHealthCheckQueueResponse()
+        {
+            Items = davItems.Select(x => new GetHealthCheckQueueResponse.HealthCheckQueueItem()
+            {
+                Name = x.Name,
+                Path = x.Path,
+                ReleaseDate = x.ReleaseDate,
+                LastHealthCheck = x.LastHealthCheck,
+                NextHealthCheck = x.NextHealthCheck,
+            }).ToList(),
+        };
+    }
+
+    protected override async Task<IActionResult> HandleRequest()
+    {
+        var request = new GetHealthCheckQueueRequest(HttpContext);
+        var response = await GetHealthCheckQueue(request);
+        return Ok(response);
+    }
+}
---
+++ backend/Api/Controllers/GetHealthCheckQueue/GetHealthCheckQueueResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace NzbWebDAV.Api.Controllers.GetHealthCheckQueue;
+
+public class GetHealthCheckQueueResponse : BaseApiResponse
+{
+    public List<HealthCheckQueueItem> Items { get; init; } = [];
+
+    public class HealthCheckQueueItem
+    {
+        public required string Name { get; init; }
+        public required string Path { get; init; }
+        public required DateTimeOffset? ReleaseDate { get; init; }
+        public required DateTimeOffset? LastHealthCheck { get; init; }
+        public required DateTimeOffset? NextHealthCheck { get; init; }
+    }
+}
