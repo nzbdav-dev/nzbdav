085bddb349696ef0fd4c61bcd6ef7026506bd699
nzbdav-dev <nzbdavdev@protonmail.com>
2025-10-20 12:38:01 -0700

    Ignore rar comment headers.

--- backend/Queue/FileProcessors/RarProcessor.cs
+++ backend/Queue/FileProcessors/RarProcessor.cs
@@ -4,10 +4,9 @@ using NzbWebDAV.Exceptions;
 using NzbWebDAV.Extensions;
 using NzbWebDAV.Queue.DeobfuscationSteps._3.GetFileInfos;
 using NzbWebDAV.Streams;
+using NzbWebDAV.Utils;
 using SharpCompress.Common;
 using SharpCompress.Common.Rar.Headers;
-using SharpCompress.IO;
-using SharpCompress.Readers;
 using Usenet.Nzb;
 
 namespace NzbWebDAV.Queue.FileProcessors;
@@ -20,32 +19,24 @@ public class RarProcessor(
 {
     public override async Task<BaseProcessor.Result?> ProcessAsync()
     {
-        try
+        await using var stream = await GetNzbFileStream();
+        var headers = await RarUtil.GetRarHeadersAsync(stream, ct);
+        return new Result()
         {
-            await using var stream = await GetNzbFileStream();
-            await using var cancellableStream = new CancellableStream(stream, ct);
-            var headers = await Task.Run(() => GetRarHeaders(cancellableStream));
-            return new Result()
-            {
-                NzbFile = fileInfo.NzbFile,
-                PartSize = cancellableStream.Length,
-                ArchiveName = GetArchiveName(),
-                PartNumber = GetPartNumber(headers),
-                StoredFileSegments = headers
-                    .Where(x => x.HeaderType == HeaderType.File)
-                    .Select(x => new StoredFileSegment()
-                    {
-                        PathWithinArchive = x.GetFileName(),
-                        Offset = x.GetDataStartPosition(),
-                        ByteCount = x.GetAdditionalDataSize(),
-                    }).ToArray(),
-                ReleaseDate = fileInfo.ReleaseDate,
-            };
-        }
-        catch (CryptographicException ex)
-        {
-            throw new PasswordProtectedRarException("Password-protected RARs are not supported");
-        }
+            NzbFile = fileInfo.NzbFile,
+            PartSize = stream.Length,
+            ArchiveName = GetArchiveName(),
+            PartNumber = GetPartNumber(headers),
+            StoredFileSegments = headers
+                .Where(x => x.HeaderType == HeaderType.File)
+                .Select(x => new StoredFileSegment()
+                {
+                    PathWithinArchive = x.GetFileName(),
+                    Offset = x.GetDataStartPosition(),
+                    ByteCount = x.GetAdditionalDataSize(),
+                }).ToArray(),
+            ReleaseDate = fileInfo.ReleaseDate,
+        };
     }
 
     private string GetArchiveName()
@@ -91,37 +82,6 @@ public class RarProcessor(
         if (endVolumeNumber != null) return endVolumeNumber!.Value;
 
         return null;
-    }
-
-    private List<IRarHeader> GetRarHeaders(Stream stream)
-    {
-        ct.ThrowIfCancellationRequested();
-        var headerFactory = new RarHeaderFactory(StreamingMode.Seekable, new ReaderOptions());
-        var headers = new List<IRarHeader>();
-        foreach (var header in headerFactory.ReadHeaders(stream))
-        {
-            ct.ThrowIfCancellationRequested();
-
-            // Add archive headers
-            if (header.HeaderType is HeaderType.Archive or HeaderType.EndArchive)
-            {
-                headers.Add(header);
-                continue;
-            }
-
-            // we only care about file headers
-            if (header.HeaderType != HeaderType.File || header.IsDirectory() || header.GetFileName() == "QO") continue;
-
-            // we only support stored files (compression method m0).
-            if (header.GetCompressionMethod() != 0)
-                throw new UnsupportedRarCompressionMethodException(
-                    "Only rar files with compression method m0 are supported.");
-
-            // add the headers
-            headers.Add(header);
-        }
-
-        return headers;
     }
 
     private async Task<NzbFileStream> GetNzbFileStream()
---
+++ backend/Utils/RarUtil.cs
@@ -0,0 +1,68 @@
+﻿using NzbWebDAV.Exceptions;
+using NzbWebDAV.Extensions;
+using NzbWebDAV.Models;
+using NzbWebDAV.Streams;
+using SharpCompress.Archives.SevenZip;
+using SharpCompress.Common;
+using SharpCompress.Common.Rar.Headers;
+using SharpCompress.IO;
+using SharpCompress.Readers;
+
+namespace NzbWebDAV.Utils;
+
+public static class RarUtil
+{
+    public static async Task<List<IRarHeader>> GetRarHeadersAsync(Stream stream, CancellationToken ct)
+    {
+        await using var cancellableStream = new CancellableStream(stream, ct);
+        return await Task.Run(() => GetRarHeaders(cancellableStream), ct);
+    }
+
+    private static List<IRarHeader> GetRarHeaders(Stream stream)
+    {
+        try
+        {
+            var headerFactory = new RarHeaderFactory(StreamingMode.Seekable, new ReaderOptions());
+            var headers = new List<IRarHeader>();
+            foreach (var header in headerFactory.ReadHeaders(stream))
+            {
+                // add archive headers
+                if (header.HeaderType is HeaderType.Archive or HeaderType.EndArchive)
+                {
+                    headers.Add(header);
+                    continue;
+                }
+
+                // skip comments
+                if (header.HeaderType == HeaderType.Service)
+                {
+                    if (header.GetFileName() == "CMT")
+                    {
+                        var buffer = new byte[header.GetCompressedSize()];
+                        _ = stream.Read(buffer, 0, buffer.Length);
+                    }
+
+                    continue;
+                }
+
+                // we only care about file headers
+                if (header.HeaderType != HeaderType.File || header.IsDirectory() ||
+                    header.GetFileName() == "QO") continue;
+
+                // we only support stored files (compression method m0).
+                if (header.GetCompressionMethod() != 0)
+                    throw new UnsupportedRarCompressionMethodException(
+                        "Only rar files with compression method m0 are supported.");
+
+                // add the headers
+                headers.Add(header);
+            }
+
+            return headers;
+        }
+        catch (CryptographicException ex)
+        {
+            throw new PasswordProtectedRarException("Password-protected RARs are not supported");
+        }
+    }
+}
