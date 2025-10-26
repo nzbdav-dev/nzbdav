204c3e1da13f47ab01386da66b9e2c24f050935c
nzbdav-dev <nzbdavdev@protonmail.com>
2025-10-26 16:53:20 -0700

    Updated default logging level to information. Prioritize app logs.

--- backend/Program.cs
+++ backend/Program.cs
@@ -30,11 +30,13 @@ class Program
     static async Task Main(string[] args)
     {
         // Initialize logger
-        var defaultLevel = LogEventLevel.Warning;
+        var defaultLevel = LogEventLevel.Information;
         var envLevel = Environment.GetEnvironmentVariable("LOG_LEVEL");
         var level = Enum.TryParse<LogEventLevel>(envLevel, true, out var parsed) ? parsed : defaultLevel;
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Is(level)
+            .MinimumLevel.Override("NWebDAV", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
             .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
             .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
             .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
@@ -109,7 +111,6 @@ class Program
         app.Services.GetRequiredService<HealthCheckService>();
 
         // run
-        app.UseSerilogRequestLogging();
         app.UseMiddleware<ExceptionMiddleware>();
         app.UseWebSockets();
         app.MapHealthChecks("/health");
