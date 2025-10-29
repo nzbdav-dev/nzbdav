3d6145b7d46ff39a9b2f189babde9cf90367de7e
nzbdav-dev <nzbdavdev@protonmail.com>
2025-10-28 21:29:07 -0700

    Updated safeguard threshold for maintenance task (#94)

--- backend/Tasks/RemoveUnlinkedFilesTask.cs
+++ backend/Tasks/RemoveUnlinkedFilesTask.cs
@@ -42,10 +42,10 @@ public class RemoveUnlinkedFilesTask(
         // get linked file paths
         Report(dryRun + $"Found {allDavItems.Count} webdav files.\nEnumerating all symlinked files...");
         var linkedIds = GetLinkedIds();
-        if (linkedIds.Count < 100)
+        if (linkedIds.Count < 5)
         {
             Report($"Aborted: " +
-                   $"There are less than one hundred symlinked files found in your library. " +
+                   $"There are less than five symlinked files found in your library. " +
                    $"Cancelling operation to prevent accidental bulk deletion.");
             return;
         }
