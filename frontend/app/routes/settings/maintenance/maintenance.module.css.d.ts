251c2241b797deefe7ff41f7ab3fe2f546794283
nzbdav-dev <nzbdavdev@protonmail.com>
2025-11-11 16:51:17 -0800

    Refactored maintenance page to make room for multiple tasks.

--- frontend/app/routes/settings/maintenance/maintenance.module.css.d.ts
+++ frontend/app/routes/settings/maintenance/maintenance.module.css.d.ts
@@ -1,13 +1,9 @@
 declare const styles: {
   readonly "container": string;
-  readonly "task": string;
-  readonly "title": string;
-  readonly "run": string;
-  readonly "run-button": string;
-  readonly "dryrun-button": string;
-  readonly "task-progress": string;
-  readonly "list": string;
-  readonly "list-item": string;
+  readonly "accordion": string;
+  readonly "accordionItem": string;
+  readonly "accordionHeader": string;
+  readonly "accordionBody": string;
 };
 export = styles;
 
---
+++ frontend/app/routes/settings/maintenance/remove-unlinked-files/remove-unlinked-files.tsx
@@ -0,0 +1,126 @@
+import { Alert, Button, Form } from "react-bootstrap";
+import styles from "./remove-unlinked-files.module.css"
+import { useCallback, useEffect, useState } from "react";
+import { receiveMessage } from "~/utils/websocket-util";
+
+const cleanupTaskTopic = { 'ctp': 'state' };
+
+type RemoveUnlinkedFilesProps = {
+    savedConfig: Record<string, string>
+};
+
+export function RemoveUnlinkedFiles({ savedConfig }: RemoveUnlinkedFilesProps) {
+    // stateful variables
+    const [connected, setConnected] = useState<boolean>(false);
+    const [progress, setProgress] = useState<string | null>(null);
+    const [isFetching, setIsFetching] = useState<boolean>(false);
+
+    // derived variables
+    const libraryDir = savedConfig["media.library-dir"];
+    const isDone = progress?.startsWith("Done");
+    const isFinished = progress?.startsWith("Done") || progress?.startsWith("Failed") || progress?.startsWith("Aborted");
+    const isRunning = !isFinished && (isFetching || progress !== null);
+    const isRunButtonEnabled = !!libraryDir && connected && !isRunning;
+    const runButtonVariant = isRunButtonEnabled ? 'success' : 'secondary';
+    const runButtonLabel = isRunning ? "⌛ Running.." : '▶ Run Task';
+
+    // effects
+    useEffect(() => {
+        let ws: WebSocket;
+        let disposed = false;
+        function connect() {
+            ws = new WebSocket(window.location.origin.replace(/^http/, 'ws'));
+            ws.onmessage = receiveMessage((_, message) => setProgress(message));
+            ws.onopen = () => { setConnected(true); ws.send(JSON.stringify(cleanupTaskTopic)); }
+            ws.onclose = () => { !disposed && setTimeout(() => connect(), 1000); setProgress(null) };
+            ws.onerror = () => { ws.close() };
+            return () => { disposed = true; ws.close(); }
+        }
+        return connect();
+    }, [setProgress, setConnected]);
+
+    // events
+    const onRun = useCallback(async () => {
+        setIsFetching(true);
+        await fetch("/api/remove-unlinked-files");
+        setIsFetching(false);
+    }, [setIsFetching]);
+
+    const onDryRun = useCallback(async (event: any) => {
+        setIsFetching(true);
+        await fetch("/api/remove-unlinked-files/dry-run");
+        setIsFetching(false);
+    }, [setIsFetching]);
+
+    // view
+    const dryRunButton =
+        <Button
+            className={styles["dryrun-button"]}
+            disabled={!isRunButtonEnabled}
+            onClick={onDryRun}
+            variant="secondary"
+            size="sm"
+        >
+            perform a dry-run
+        </Button>;
+
+    return (
+        <>
+            {!libraryDir &&
+                <Alert className={styles.alert} variant="warning">
+                    Warning
+                    <ul className={styles.list}>
+                        <li className={styles["list-item"]}>
+                            You must first configure the Library Directory setting before running this task.
+                            Head over to the Repairs tab.
+                        </li>
+                    </ul>
+                </Alert>
+            }
+            {libraryDir &&
+                <Alert className={styles.alert} variant="danger">
+                    <span style={{ fontWeight: 'bold' }}>Danger</span>
+                    <ul className={styles.list}>
+                        <li className={styles["list-item"]}>
+                            Make a backup of your NzbDAV database prior to running this task
+                        </li>
+                        <li className={styles["list-item"]}>
+                            Files will be removed from the webdav and will not be recoverable without a backup
+                        </li>
+                    </ul>
+                </Alert>
+            }
+            <div className={styles.task}>
+                <Form.Group>
+                    <div className={styles.run}>
+                        <Button
+                            className={styles["run-button"]}
+                            variant={runButtonVariant}
+                            onClick={onRun}
+                            disabled={!isRunButtonEnabled}
+                        >
+                            {runButtonLabel}
+                        </Button>
+                        <div className={styles["task-progress"]}>
+                            {progress}
+                            {isDone && <>
+                                &nbsp;<a href="/api/remove-unlinked-files/audit">Audit.</a>
+                            </>}
+                        </div>
+                    </div>
+                    <Form.Text id="cleanup-task-progress-help" muted>
+                        <br />
+                        This task will scan your organized media library for all symlinked or *.strm linked files.
+                        Any file on the webdav that is not pointed to by your library will be deleted.
+                        If you would like to see what would be deleted without running the task, you can {dryRunButton}.
+                        The dry-run will not delete anything.
+                        <br />
+                        <br />
+                        Note: This task is intended to cleanup old, abandoned, files.
+                        Files added less than twenty-four hours ago will not be deleted.
+                    </Form.Text>
+                </Form.Group>
+            </div>
+        </>
+    );
+}
